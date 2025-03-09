9af490ec483180f30228d0383fd65e0557d32a99
Barbara Jeziorek <b.jeziorek@comfortel.pl>
2025-03-09 17:18:28 +0100

    types, data formatting, displaying date, styles

--- frontend/txt-app/src/app/data-tables/data-tables.component.ts
+++ frontend/txt-app/src/app/data-tables/data-tables.component.ts
@@ -1,30 +1,73 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common'
+import { sceny } from '../data';
 
-type Schema = any;
+
+type Char = {
+  name:string,
+  emotions:string,
+  goal:string,
+  other:string[]
+}
+
+type DateKryst = {
+  year:number,
+  month:number,
+  day:number,
+  hour:number,
+  desc:string,
+  [key: string]: any;
+}
+
+export type Schema = {
+  "nr": number,
+  "descTitle":string,
+  "shortDesc":string,
+  "date":DateKryst,
+  "characters":Char[],
+  "pov":string,
+  "place":string[],
+  "didascalia":string,
+  "remarks":string,
+   [key: string]: any;
+};
 
 @Component({
   selector: 'app-data-tables',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './data-tables.component.html',
   styleUrl: './data-tables.component.css'
 })  
 export class DataTablesComponent {
   data$ = Observable<any>;
+sceny = sceny;
+
+public showhide = true;
+
+ toggle(){
+  this.showhide=!this.showhide;
+ }
+
+  get headers() {
+    return Object.keys(sceny[0]);
+  }
+
+  trackByFn(index: number, item: Schema): number {
+    return item.nr;
+  }
+ 
 
   constructor(private api: ApiService) { }
 
 
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-   // this.data$=this.api.getData;
    this.api.getData().subscribe(
     (response:Schema) => {
       console.log(response);
-    }
+    }    
   );
   }
 }
---
+++ frontend/txt-app/src/app/short-desc/short-desc.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-short-desc',
+  imports: [],
+  templateUrl: './short-desc.component.html',
+  styleUrl: './short-desc.component.css'
+})
+export class ShortDescComponent {
+
+}
