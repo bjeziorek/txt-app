ae08ce9da1f919dda59635021bda90aa4862d3dc
Barbara Jeziorek <barbarajeziorek@gmail.com>
2025-03-17 22:20:03 +0100

    filter modes

--- frontend/txt-app/src/app/data-tables/data-tables.component.ts
+++ frontend/txt-app/src/app/data-tables/data-tables.component.ts
@@ -4,22 +4,28 @@ import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common'
 import { sceny } from '../data';
 import { Schema } from '../txt.model';
+import { FormsModule } from '@angular/forms';
 
-enum MyEnum {
-  A = "A",
-  B = "B",
-  C = "C"
+enum FilterModes {
+  Suma = "Suma",
+  Mnożenie = "Mnożenie",
 }
 
 @Component({
   selector: 'app-data-tables',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './data-tables.component.html',
   styleUrl: './data-tables.component.css'
 })  
 export class DataTablesComponent {
+
+onFilterModeChange() {
+  this.filterScenesByCharacter('')
+//throw new Error('Method not implemented.');
+}
   public characters:string[]=[];
-  example: MyEnum = MyEnum.A;
+  public filters:{character:string;filter:boolean}[]=[];
+  public filterMode:FilterModes=FilterModes.Suma
 
   data$ = Observable<any>;
 scenes = sceny.sort((a, b) => {
@@ -30,13 +36,129 @@ scenes = sceny.sort((a, b) => {
   return 0;
 });
 
+isFilterOnForChar(name:string){
+  const z=this.filters.filter(filter=>filter.character && filter.filter).map(el=>el.character).includes(name)
+ // console.log('zzzzzzzzzzz',z)
+  return z
+  //.includes[name]
+}
+
 filterScenesByCharacter(character: string){
-  if(character===''){
+  this.isFilterOnForChar("Fei")
+  //console.log('-------------1',this.filters)
+  this.filters = this.filters.map(
+    filter=>{
+   //   console.log(' filter.character==character', character, filter.character==character, filter)
+      if( filter.character==character){
+       
+        return {character, filter:!filter.filter}
+      }else{
+        return filter
+      }
+      // filter.character==character? ({character, filter:!filter}):filter
+      // return 
+    })
+  //console.log('-------------2', this.filters)
+
+  const filtersOn = this.filters.filter(filter=>filter.filter).map(el=>el.character)
+  this.polaczTablice(  sceny.filter(scene => 
+    filtersOn.some(filter => 
+      scene.characters.some(character => character.name === filter)
+    )
+  ))
+//  console.log('-------------3', filtersOn)
+  // dotąd działa, a dalej jest jakies bezowisko... musze zebrac sceny
+
+  // tu coś ten filtr nie działa
+//[[filter is on to np 'tian', ;fei
+
+  // switch(this.filterMode){
+  //   case FilterModes.Suma:
+  //   console.log('in suma') 
+  //   this.scenes=sceny.filter(scene => {
+  //       console.log('............scena:',scene)
+  //       return filtersOn.map(filter=>{
+  //         console.log('...............filter',filter)
+  //         return scene.characters.some(char=>{
+  //           if( filter===char.name) console.log('............',char, filter, char.name, filter===char.name)
+  //           return filter==char.name
+  //         })
+  //       })
+  //     })
+  //   break;
+  //   case FilterModes.Mnożenie:
+  //     console.log('in mnożenie') 
+  //     // this.scenes=sceny.filter(scene => {
+  //     //   return filtersOn.map(filter=>{
+  //     //     return scene.characters.some(char=>filter==char.name)
+  //     //   })
+  //     // }).filter(scene=>{
+  //     //   filtersOn.every(filter=>{
+  //     //     scene.characters.map(ch=>ch.name).includes(filter);
+  //     //   })
+  //     // })
+  //   break;
+  //   default:
+  //     console.error('nieznany filter mode:', this.filterMode)
+  // }
+ 
+
+
+  
+
+
+  
+  //console.log('filtersOn',filtersOn, "filters",this.filters, 'scenes',this.scenes)
+  // if(this.filters.find(filter=>filter.character==character && !filter.filter)) return
+
+  if(character==='ALL'){
     this.scenes=sceny;
+    this.filters = this.filters.map(filter=>{
+      return {
+        character:filter.character,
+        filter:false
+      }
+  });
     return;
   }
+  //console.log(this.scenes.length, this.scenes)
+  //this.scenes=sceny.filter(scene => scene.characters.filter(char=>char.name===character).length>0)
+  //this.polaczTablice(this.scenes, sceny.filter(scene => scene.characters.filter(char=>char.name===character).length>0))
+
+  const sceneList: any[] = []
+
+  filtersOn.forEach(filter=>{
+    sceneList.push(
+      sceny.filter(scene => scene.characters.filter(char=>char.name===filter).length>0)
+    )
+  })
+
+
+  switch(this.filterMode){
+    case FilterModes.Suma:
+    console.log('in suma') 
+    this.scenes=sceny.filter(scene => scene.characters.filter(char=>char.name===character).length>0)
+    this.scenes=sceneList.flat();
+    console.log('suma',this.scenes)
+    break;
+    case FilterModes.Mnożenie:
+      console.log('in mnożenie') 
+      this.scenes=sceny.filter(scene => 
+        // tylko taka scena ktora zawiera wszystkie postacie z listy
+        filtersOn.every(filter=>scene.characters.some(char=>char.name===filter)))
+//        scene.characters.every(char=>filtersOn.includes))
+     // this.scenes=sceneList.flat();
+      console.log('mno',this.scenes)
+    break;
+    default:
+      console.error('nieznany filter mode:', this.filterMode)
+  }
+
   console.log(this.scenes.length, this.scenes)
-  this.scenes=sceny.filter(scene => scene.characters.filter(char=>char.name===character).length>0)
+  // this.scenes=sceny.filter(scene => scene.characters.filter(char=>char.name===character).length>0)
+  // this.scenes=sceneList.flat();
+  
+
   console.log(this.scenes.length, this.scenes)
 }
 
@@ -44,10 +166,17 @@ public showhide = true;
 
  toggle(){
   this.showhide=!this.showhide;
-
-  this.example = MyEnum.B
-
  }
+
+ polaczTablice(...tablice: Schema[][]): Schema[] {
+  return [
+    ...new Map(
+      tablice
+        .flat() // Spłaszczenie wszystkich tablic w jedną
+        .map((obiekt) => [`${obiekt.nr}_${obiekt.pov}`, obiekt])
+    ).values()
+  ];
+};
 
   testSendScene(item:Schema){
     console.log('testSendScene:item',item)
@@ -75,9 +204,17 @@ public showhide = true;
   return uniqueNames;
   }
 
+  generateCharacterFilters(){
+    this.filters = this.characters.map(char=>({
+      character: char,
+      filter: false
+    }))
+  }
+
   constructor(private api: ApiService) { 
     this.characters=this.generateCharacterEnum(sceny);
-
+    this.generateCharacterFilters();
+    console.log("filters",this.filters)
   }
 
 
