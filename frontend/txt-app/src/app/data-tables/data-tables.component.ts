12471bbfa59f00c26e3917664b424363a4f84b7e
Barbara Jeziorek <barbarajeziorek@gmail.com>
2025-03-23 20:12:00 +0100

    cleaning code

--- frontend/txt-app/src/app/data-tables/data-tables.component.ts
+++ frontend/txt-app/src/app/data-tables/data-tables.component.ts
@@ -21,7 +21,6 @@ export class DataTablesComponent {
 
 onFilterModeChange() {
   this.filterScenesByCharacter('')
-//throw new Error('Method not implemented.');
 }
   public characters:string[]=[];
   public filters:{character:string;filter:boolean}[]=[];
@@ -38,27 +37,18 @@ scenes = sceny.sort((a, b) => {
 
 isFilterOnForChar(name:string){
   const z=this.filters.filter(filter=>filter.character && filter.filter).map(el=>el.character).includes(name)
- // console.log('zzzzzzzzzzz',z)
   return z
-  //.includes[name]
 }
 
 filterScenesByCharacter(character: string){
-  this.isFilterOnForChar("Fei")
-  //console.log('-------------1',this.filters)
   this.filters = this.filters.map(
     filter=>{
-   //   console.log(' filter.character==character', character, filter.character==character, filter)
       if( filter.character==character){
-       
         return {character, filter:!filter.filter}
       }else{
         return filter
       }
-      // filter.character==character? ({character, filter:!filter}):filter
-      // return 
     })
-  //console.log('-------------2', this.filters)
 
   const filtersOn = this.filters.filter(filter=>filter.filter).map(el=>el.character)
   this.polaczTablice(  sceny.filter(scene => 
@@ -66,50 +56,7 @@ filterScenesByCharacter(character: string){
       scene.characters.some(character => character.name === filter)
     )
   ))
-//  console.log('-------------3', filtersOn)
-  // dotąd działa, a dalej jest jakies bezowisko... musze zebrac sceny
 
-  // tu coś ten filtr nie działa
-//[[filter is on to np 'tian', ;fei
-
-  // switch(this.filterMode){
-  //   case FilterModes.Suma:
-  //   console.log('in suma') 
-  //   this.scenes=sceny.filter(scene => {
-  //       console.log('............scena:',scene)
-  //       return filtersOn.map(filter=>{
-  //         console.log('...............filter',filter)
-  //         return scene.characters.some(char=>{
-  //           if( filter===char.name) console.log('............',char, filter, char.name, filter===char.name)
-  //           return filter==char.name
-  //         })
-  //       })
-  //     })
-  //   break;
-  //   case FilterModes.Mnożenie:
-  //     console.log('in mnożenie') 
-  //     // this.scenes=sceny.filter(scene => {
-  //     //   return filtersOn.map(filter=>{
-  //     //     return scene.characters.some(char=>filter==char.name)
-  //     //   })
-  //     // }).filter(scene=>{
-  //     //   filtersOn.every(filter=>{
-  //     //     scene.characters.map(ch=>ch.name).includes(filter);
-  //     //   })
-  //     // })
-  //   break;
-  //   default:
-  //     console.error('nieznany filter mode:', this.filterMode)
-  // }
- 
-
-
-  
-
-
-  
-  //console.log('filtersOn',filtersOn, "filters",this.filters, 'scenes',this.scenes)
-  // if(this.filters.find(filter=>filter.character==character && !filter.filter)) return
 
   if(character==='ALL'){
     this.scenes=sceny;
@@ -121,9 +68,6 @@ filterScenesByCharacter(character: string){
   });
     return;
   }
-  //console.log(this.scenes.length, this.scenes)
-  //this.scenes=sceny.filter(scene => scene.characters.filter(char=>char.name===character).length>0)
-  //this.polaczTablice(this.scenes, sceny.filter(scene => scene.characters.filter(char=>char.name===character).length>0))
 
   const sceneList: any[] = []
 
@@ -136,30 +80,16 @@ filterScenesByCharacter(character: string){
 
   switch(this.filterMode){
     case FilterModes.Suma:
-    console.log('in suma') 
     this.scenes=sceny.filter(scene => scene.characters.filter(char=>char.name===character).length>0)
     this.scenes=sceneList.flat();
-    console.log('suma',this.scenes)
     break;
     case FilterModes.Mnożenie:
-      console.log('in mnożenie') 
       this.scenes=sceny.filter(scene => 
-        // tylko taka scena ktora zawiera wszystkie postacie z listy
         filtersOn.every(filter=>scene.characters.some(char=>char.name===filter)))
-//        scene.characters.every(char=>filtersOn.includes))
-     // this.scenes=sceneList.flat();
-      console.log('mno',this.scenes)
     break;
     default:
       console.error('nieznany filter mode:', this.filterMode)
   }
-
-  console.log(this.scenes.length, this.scenes)
-  // this.scenes=sceny.filter(scene => scene.characters.filter(char=>char.name===character).length>0)
-  // this.scenes=sceneList.flat();
-  
-
-  console.log(this.scenes.length, this.scenes)
 }
 
 public showhide = true;
@@ -172,14 +102,13 @@ public showhide = true;
   return [
     ...new Map(
       tablice
-        .flat() // Spłaszczenie wszystkich tablic w jedną
+        .flat()
         .map((obiekt) => [`${obiekt.nr}_${obiekt.pov}`, obiekt])
     ).values()
   ];
 };
 
   testSendScene(item:Schema){
-    console.log('testSendScene:item',item)
     this.api.updateSceneById(item)
   }
 
@@ -192,7 +121,6 @@ public showhide = true;
   }
  
   generateCharacterEnum(data: Schema[]){
-    // const uniqueNames = Array.from(new Set(data.map(item => item.characters.name)));
   
     const uniqueNames = Array.from(
       new Set(
@@ -214,7 +142,6 @@ public showhide = true;
   constructor(private api: ApiService) { 
     this.characters=this.generateCharacterEnum(sceny);
     this.generateCharacterFilters();
-    console.log("filters",this.filters)
   }
 
 
