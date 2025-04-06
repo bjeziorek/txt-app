3a5a57438389679dd9604504f8c1443d2b9f2d7c
Barbara Jeziorek <barbarajeziorek@gmail.com>
2025-04-06 15:56:51 +0200

    openProject api, createProject api

--- backend/src/api/translation/utils/parseProperties.ts
+++ backend/src/api/translation/utils/parseProperties.ts
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import path from 'path';
 
 export const parsePropertiesFile = async (filePath: string): Promise<Record<string, string>> => {
     return new Promise((resolve, reject) => {
